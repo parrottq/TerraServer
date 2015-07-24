1c08eef4d1c9269bb04edefbc2dd389b6dad444a
Quinn Parrott <quinn.parrott@gmail.com>
2015-07-24 09:36:35 -0600

    Simplified if statements

--- main.py
+++ main.py
@@ -7,26 +7,26 @@ import BaseHTTPServer
 from urlparse import urlparse
 
 
-#Terraria Server controller
+# Terraria Server controller
 class Server:
-    #Initializer
+    # Initializer
     def __init__(self):
         self.isOn = False
         self.port = 7777
 
     def handleCommands(self, command):
         if command[:6] == "start_":
-            if self != None:
+            if self is not None:
                 print self.getIsOn()
-                if self.getIsOn() == True:
+                if self.getIsOn():
                     self.stop()
             if command[6] != 'n' and command[6] != 'd':
                 self.start(command[6:])
         else:
             self.console(command)
 
-    #Reads from servers console then log it
-    #Run in seperate thread
+    # Reads from servers console then log it
+    # Run in seperate thread
     def readline(self):
         while True:
             text = self.server.stdout.readline()
@@ -34,39 +34,39 @@ class Server:
             if text != "":
                 if text[0] == "<":
                     name = text[1:text.index(">")]
-                    print name + " command: " + text[text.index(">")+2:]
-                    if text[text.index(">")+2] == "/":
+                    print name + " command: " + text[text.index(">") + 2:]
+                    if text[text.index(">") + 2] == "/":
                         players = ["A Bum", "The One"]
                         if any(name in s for s in players):
                             print "handled"
-                            self.handleCommands(text[text.index(">")+3:])
+                            self.handleCommands(text[text.index(">") + 3:])
                 else:
-                    self.server.stdin.write("say "+text)
+                    self.server.stdin.write("say " + text)
             time.sleep(0.01)
 
-    #Writes to the console
+    # Writes to the console
     def console(self, command):
         if "exit" in command:
             self.isOn = False
-        self.server.stdin.write(command+"\n")
+        self.server.stdin.write(command + "\n")
 
-    #Starts server
+    # Starts server
     def start(self, world):
         self.isOn = True
         self.server = subprocess.Popen(
-                ["TerrariaServer"],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE
-            )
-        
+            ["TerrariaServer"],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE
+        )
+
         self.readerthread = threading.Thread(target=self.readline)
         self.readerthread.start()
 
         time.sleep(1)
-        self.console(str(world)+"\n\n"+str(self.port)+"\n\n\n")
+        self.console(str(world) + "\n\n" + str(self.port) + "\n\n\n")
 
     def stop(self):
-        if self.isOn == True:
+        if self.isOn:
             self.server.stdin.write("exit\n")
         self.isOn = False
         time.sleep(2)
@@ -80,20 +80,20 @@ class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def handleCommands(self, command):
         if command[:6] == "start_":
-            if self.serverTerraria != None:
+            if self.serverTerraria is not None:
                 print self.serverTerraria.getIsOn()
-                if self.serverTerraria.getIsOn() == True:
+                if self.serverTerraria.getIsOn():
                     self.serverTerraria.stop()
             if command[6] != 'n' and command[6] != 'd':
                 self.serverTerraria.start(command[6:])
         else:
             self.serverTerraria.console(command)
-    
+
     def isPassword(self, password):
         if password == "pass":
             return True
         return False
-    
+
     def do_HEAD(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
@@ -105,12 +105,12 @@ class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         if self.isPassword(self.url.query):
             page = open("data.html", 'r')
             self.wfile.write(page.read())
-            print page.read()
             page.close()
-            
+
             self.handleCommands(self.url.path[1:])
         else:
             self.wfile.write("Incorrect password")
+
 
 class HttpServer:
     def start(self):
@@ -122,7 +122,7 @@ class HttpServer:
         self.serverHttp.server_close()
 
 
-#server = Server()
+# server = Server()
 
 if __name__ == "__main__":
     b = HttpServer()
