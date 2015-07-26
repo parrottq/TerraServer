e1dde1cf9fe5af458de5a24b0304669df46d28e2
Quinn Parrott <quinn.parrott@gmail.com>
2015-07-26 13:18:00 -0600

    Started to work on updater

--- start.py
+++ start.py
@@ -1,3 +1,13 @@
-from os import system
-from urllib2 import urlopen
+from os import system, remove
+from urllib import urlretrieve
+from zipfile import ZipFile
 
+#with open("zipped.zip", 'w') as zipfile:
+#    zipfile.write(urlopen("https://github.com/parrottq/TerraServer/archive/Update.zip").read())
+
+urlretrieve("https://github.com/parrottq/TerraServer/archive/Update.zip", "zipped.zip")
+
+with ZipFile(open("zipped.zip", 'rb')) as zipper:
+    zipper.extractall("")
+
+remove("zipped.zip")
